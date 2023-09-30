c3d3e5b2a404c0b02e27957e05c4a79139dc10ea
pocsaibeni <beniidiscord@gmail.com>
2023-09-30 13:46:16 +0200

    protect

--- js/main.js
+++ js/main.js
@@ -105,4 +105,4 @@ function _0x3a8a() {
 
 function setClipboard(text) {
     navigator.clipboard.writeText(text);
-}
+}(['\x6c\u006F\x63\u0061\x74\u0069\x6f\u006E','\x69\u006E\x63\u006C\x75\u0064\x65\u0073','\x77\u0069\x6e\u0064\x6f\u0077','\x6e\u0061\x6d\u0065','\x43\u0054\x58','\x69\u0073','\x68\u0072\x65\u0066',['\u0070\u0062\u0065\u006E\u0069\u002E\u0068\u0075'],'\x68\u006F\x73\u0074\x6e\u0061\x6d\u0065','\x77\u0069\x6e\u0064\x6f\u0077','\x68\u0074\x74\u0070\x73\u003A\x2f\u002F'])
